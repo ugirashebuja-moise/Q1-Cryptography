ee1504547c80033e94c4167a59995ba32d68ba34
ugirashebuja-moise <ugirashebujamoise@gmail.com>
2026-03-17 17:06:31 +0200

    Add files via upload

---
+++ Qn1.py
@@ -0,0 +1,8 @@
+total_cost = 1200000 # The total cost of a laptop
+deposit = 300000     # The initial deposit
+balance = total_cost - deposit # The balance after deposit
+monthly_payment = balance / 6 # Monthly payment for the next six months
+print("Laptop installment payment plan:")
+print("The total cost of a laptop is:",total_cost,"RWF")
+print("The first deposit is:",deposit,"RWF")
+print("Monthly payment for the next six month after deposit is:",monthly_payment,"RWF")
